2b60cb6bca299f17ff168e929803a1c3cc037093
hridoythebest <hridoythebest@gmail.com>
2025-02-10 21:56:41 +0600

    shadcn updated

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -22,7 +22,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { toast } from "@/components/ui/use-toast";
+import { toast } from "../../components/ui/use-toast";
 import { Building2, Mail, MessageSquare, Phone } from "lucide-react";
 
 const formSchema = z.object({
